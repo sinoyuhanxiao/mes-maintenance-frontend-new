a493d9655422fab19475ed8b629f9943a9a58842
FPSYL <108029632+FPSYL@users.noreply.github.com>
2025-08-22 16:42:45 -0700

    T4 create edit delete (#27)
    * Add and Delete Tier 3
    
    * Add and Delete Tier 3
    
    * Add, Edit, and Delete Tier 3
    
    * Add, Edit, and Delete Tier 3
    
    * Add t4 CRUD
    
    * Add t4 CRUD
    
    * Update .env.development
    
    ---------
    
    Co-authored-by: Erik Yu <147850688+erikyu01@users.noreply.github.com>

--- src/views/equipment/components/subEquipment/index.vue
+++ src/views/equipment/components/subEquipment/index.vue
@@ -6,29 +6,71 @@
           {{ item.label }}
         </el-breadcrumb-item>
       </el-breadcrumb>
-      <h1>{{ node.label }}</h1>
+      <div class="t4-sub-header">
+        <h1>{{ node.label }}</h1>
+        <el-dropdown class="kebab-dropdown">
+          <el-icon class="kebab-icon">
+            <MoreFilled />
+          </el-icon>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item @click="openAddDialog">Add New Tier 4</el-dropdown-item>
+              <el-dropdown-item @click="openEditDialog">Edit Tier 4</el-dropdown-item>
+              <el-dropdown-item divided @click="openDeactivateDialog">Delete Tier 4</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+      </div>
     </div>
     <div class="t4-main-details">
-      <el-tabs v-model="activeTab" type="border-card">
+      <el-tabs v-model="activeTab" type="border-card" :key="refreshKey">
         <el-tab-pane label="Details" name="details">
-          <SubEquipmentDetailsTab :equipmentId="props.node.id" />
+          <SubEquipmentDetailsTab :equipmentId="props.node.id" :key="`details-${refreshKey}`" />
         </el-tab-pane>
         <el-tab-pane label="Sub Items" name="subItems">
-          <SubItemsTab />
+          <SubItemsTab :key="`subitems-${refreshKey}`" />
         </el-tab-pane>
         <el-tab-pane label="Maintenance History" name="maintenanceHistory">
-          <MaintenanceHistory />
+          <MaintenanceHistory :key="`maintenance-${refreshKey}`" />
         </el-tab-pane>
       </el-tabs>
     </div>
+
+    <el-dialog v-model="showAddDialog" title="Add New Tier 4" width="600px" :before-close="handleCloseDialog">
+      <AddSubEquipment v-if="showAddDialog" @close="closeAddDialog" @success="handleAddSuccess" :parentId="parentId" />
+    </el-dialog>
+
+    <el-dialog v-model="showEditDialog" title="Edit Tier 4" width="600px" :before-close="handleCloseDialog">
+      <EditSubEquipment
+        v-if="showEditDialog"
+        :key="`edit-${props.node.id}-${editDialogKey}`"
+        @close="closeEditDialog"
+        @success="handleEditSuccess"
+        :equipmentId="props.node.id"
+      />
+    </el-dialog>
+
+    <el-dialog v-model="showDeactivateDialog" title="Delete Tier 4" width="600px" :before-close="handleCloseDialog">
+      <DeactivateNode
+        v-if="showDeactivateDialog"
+        @close="closeDeactivateDialog"
+        @success="handleDeleteSuccess"
+        @refresh-tree="handleRefreshTree"
+        :equipmentId="props.node.id"
+      />
+    </el-dialog>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import SubEquipmentDetailsTab from './SubEquipmentDetails.vue'
+import { ref, computed } from 'vue'
+import { MoreFilled } from '@element-plus/icons-vue'
+import SubEquipmentDetailsTab from './Details.vue'
 import SubItemsTab from './SubItems.vue'
 import MaintenanceHistory from './MaintenanceHistory.vue'
+import AddSubEquipment from './components/AddSubEquipment.vue'
+import EditSubEquipment from './components/EditSubEquipment.vue'
+import DeactivateNode from '../common/DeactivateNode.vue'
 
 const props = defineProps( {
   node : {
@@ -41,11 +83,88 @@ const props = defineProps( {
   }
 } )
 
+const emit = defineEmits( ['refresh-tree', 'refresh-data'] )
+
+const parentId = computed( () => {
+  const validBreadcrumbItems = props.breadcrumb.filter( ( item, index ) => {
+    return index > 0 && item && typeof item === 'object' && 'id' in item
+  } )
+
+  if ( validBreadcrumbItems.length >= 2 ) {
+    return validBreadcrumbItems[validBreadcrumbItems.length - 2].id
+  }
+
+  return null
+} )
+
 const activeTab = ref( 'details' )
+const showAddDialog = ref( false )
+const showEditDialog = ref( false )
+const showDeactivateDialog = ref( false )
+const refreshKey = ref( 0 )
+const editDialogKey = ref( 0 )
+
+const openAddDialog = () => {
+  showAddDialog.value = true
+}
+
+const closeAddDialog = () => {
+  showAddDialog.value = false
+}
+
+const openEditDialog = () => {
+  editDialogKey.value += 1
+  showEditDialog.value = true
+}
+
+const closeEditDialog = () => {
+  showEditDialog.value = false
+}
+
+const openDeactivateDialog = () => {
+  showDeactivateDialog.value = true
+}
+
+const closeDeactivateDialog = () => {
+  showDeactivateDialog.value = false
+}
+
+const handleCloseDialog = done => {
+  done()
+}
+
+const handleAddSuccess = newEquipment => {
+  closeAddDialog()
+  emit( 'refresh-tree' )
+}
+
+const handleEditSuccess = updatedEquipment => {
+  closeEditDialog()
+  emit( 'refresh-tree' )
+
+  setTimeout( () => {
+    refreshViewData()
+  }, 100 )
+}
+
+const refreshViewData = () => {
+  refreshKey.value += 1
+  emit( 'refresh-data', props.node.id )
+}
+
+const handleDeleteSuccess = deletedEquipmentId => {
+  closeDeactivateDialog()
+  emit( 'refresh-tree' )
+}
+
+const handleRefreshTree = () => {
+  emit( 'refresh-tree' )
+}
 </script>
 
 <style scoped>
 .t4-main-container {
+  flex: 1;
   display: flex;
   flex-direction: column;
 }
@@ -57,9 +176,35 @@ const activeTab = ref( 'details' )
   padding: 0px;
 }
 
+.t4-sub-header {
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
 .t4-main-details {
   flex: 1;
   display: flex;
   flex-direction: column;
+}
+
+.kebab-dropdown {
+  margin-left: auto;
+}
+
+.kebab-icon {
+  transform: rotate(90deg);
+  cursor: pointer;
+  padding: 8px;
+  border-radius: 4px;
+  transition: background-color 0.2s ease;
+  font-size: 24px;
+  color: #409eff;
+}
+
+.kebab-icon:hover {
+  background-color: var(--el-fill-color-light);
 }
 </style>
